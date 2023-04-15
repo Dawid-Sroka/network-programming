11d550d0d0a9f6432fe1b6b516df87329fe65968
Dawid-Sroka <dawid.sroka.s@gmail.com>
2023-04-15 11:46:51 +0200

    add header guard

--- Traceroute/header.h
+++ Traceroute/header.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <stdint.h>
 #include <netinet/ip.h>
 #include <netinet/ip_icmp.h>
@@ -13,3 +16,5 @@
 uint16_t compute_icmp_checksum (const void *buff, int length);
 int decode(u_int8_t* buffer, int id, int ttl);
 void print_reply_addrs(uint32_t senders_ip_raw[], int senders_bitmap[]);
+
+#endif // HEADER_H
