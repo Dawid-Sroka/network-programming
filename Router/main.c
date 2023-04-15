6db97a46bc6f29169eed6b350230c6ab1683d18f
Dawid-Sroka <dawid.sroka.s@gmail.com>
2023-04-15 12:10:36 +0200

    Router skeleton

---
+++ Router/main.c
@@ -0,0 +1,21 @@
+// Dawid Sroka, 317239
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <arpa/inet.h>
+
+// #include "header.h"
+
+
+int main() {
+	return EXIT_SUCCESS;
+}
