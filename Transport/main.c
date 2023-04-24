fe12c0807663e15fa89af381bbbe2e2ffd8cf63e
Dawid-Sroka <dawid.sroka.s@gmail.com>
2023-04-25 01:53:20 +0200

    Try to add writing to output file

--- Transport/main.c
+++ Transport/main.c
@@ -9,7 +9,8 @@
 // #define BUF_SIZE 100
 #define MAX_PACKET_SIZE 1000
 
-char buffer[1500];	// buffer for received replies
+char buffer[SWS][MAX_PACKET_SIZE];	// buffer for received replies
+char header_buffer[40 + MAX_PACKET_SIZE];
 
 
 
@@ -70,6 +71,7 @@ int main(int argc, char* argv[]) {
 			for (int i = 0; i < SWS; i++)
 			{
 				if ( iter->state == 1) {
+					dprintf(outfd, buffer[i]);
 					iter->state = 0;	// reset state
 					iter->pstart += SWS * MAX_PACKET_SIZE;  
 					iter = CIRCLEQ_LOOP_NEXT(&head, iter, link);
@@ -78,19 +80,8 @@ int main(int argc, char* argv[]) {
 				}
 			}
 			
-
-			// while (next != (void *)&winfst && next->state == 1) {
-			// 	iter->pstart += SWS * MAX_PACKET_SIZE;  
-			// 	// free(iter);
-			// 	iter = next;
-			// 	next = CIRCLEQ_NEXT(iter, link);
-			// }
-
-			// take the one after, which is the first packet that didn't receive answer (state == 0)
-			// iter->state = 0;
-			// winfst = CIRCLEQ_NEXT(iter, link);
 			winfst = iter;
-		print_q(&head, winfst);
+			print_q(&head, winfst);
 			expecting = winfst->pstart;
 			fprintf(stdout, "expecting = %d\n", expecting);
 
@@ -104,7 +95,7 @@ int main(int argc, char* argv[]) {
 
 			int position = expecting;
 			for(int i = 0; i < SWS; i++){
-				if (np->state == 0 ) {
+				if (np->state == 0 && np->pstart < size) {
 					// char* mes = "GET 0 1000\n";
 					// strcat(mes, itoa(position));
 					int messlen = 11 + how_long(position); 
@@ -116,19 +107,7 @@ int main(int argc, char* argv[]) {
 				position += MAX_PACKET_SIZE;
 				np = CIRCLEQ_LOOP_NEXT(&head, np, link);
 			}
-
-			// CIRCLEQ_FOREACH(np, &head, link){
-			// 	if (np->state == 0 ) {
-			// 		// char* mes = "GET 0 1000\n";
-			// 		// strcat(mes, itoa(position));
-			// 		int messlen = 11 + how_long(position); 
-			// 		char mes[messlen];
-			// 		snprintf(mes, messlen, "GET %d 1000\n", position);
-			// 		printf("%s", mes); // outputs so you can see it
-			// 		send(sockfd, mes, strlen(mes), 0);
-			// 	}
-			// 		position+=1000;
-			// }	
+	
 		}
 
 
@@ -136,14 +115,11 @@ int main(int argc, char* argv[]) {
 		fd_set descriptors;
 		FD_ZERO (&descriptors);
 		FD_SET (sockfd, &descriptors);
-		struct timeval tv; tv.tv_sec = 0; tv.tv_usec = 1000000;
+		struct timeval tv; tv.tv_sec = 0; tv.tv_usec = 100000;
 
 		while (1)
 		{
-			// fprintf(stdout, "inner loop\n");
-
 			// select + recvfrom -> wait max 1s
-			// fprintf(stdout, "time left = %ld\n", tv.tv_usec);
 
 			int ready = select (sockfd+1, &descriptors, NULL, NULL, &tv);
 			if(ready == 0){	// timeout expired and no packet came
@@ -159,7 +135,7 @@ int main(int argc, char* argv[]) {
 			struct sockaddr_in sender;
 			socklen_t sender_len = sizeof(sender);
 
-			ssize_t packet_len = recvfrom (sockfd, buffer, IP_MAXPACKET,
+			ssize_t packet_len = recvfrom (sockfd, header_buffer, 40 + IP_MAXPACKET,
 							MSG_DONTWAIT, (struct sockaddr*)&sender, &sender_len);
 			if (packet_len < 0) {
 				fprintf(stderr, "recvfrom error: %s\n", strerror(errno));
@@ -169,19 +145,26 @@ int main(int argc, char* argv[]) {
 
 				int res_start = 0, res_len = 0;
 				char p[10];
-				sscanf(buffer, "%s %d %d", p, &res_start, &res_len);
-				fprintf(stdout, "got %d %d\n", res_start, res_len);
+				sscanf(header_buffer, "%s %d %d", p, &res_start, &res_len);
+				// fprintf(stdout, "got %d %d\n", res_start, res_len);
 
 				if (expecting <= res_start && res_start < expecting + SWS * MAX_PACKET_SIZE) {
-					// fprintf(stdout, "res %d in the window\n", res_start);
 
 					int index = (res_start - expecting) / MAX_PACKET_SIZE;
 					struct packet* iter = winfst;
 					for (int i = 0; i < index; i++)
 						iter = CIRCLEQ_LOOP_NEXT(&head, iter, link);
 
-					iter->state = 1;
-					iter->pstart = expecting + index * MAX_PACKET_SIZE;
+					if(iter->state == 0) {
+						fprintf(stdout, "res %d in the window\n", res_start);
+						int start_len = how_long(res_start);
+						int len_len = how_long(res_len);
+						memcpy(buffer[index], header_buffer + 4 + 1 + start_len + 1 + len_len + 1, MAX_PACKET_SIZE);
+						iter->state = 1;
+						iter->pstart = expecting + index * MAX_PACKET_SIZE;
+						// write(outfd, header_buffer, MAX_PACKET_SIZE);
+					}
+
 
 					if (index == 0) {
 						fprintf(stdout, "index = 0\n");
@@ -193,9 +176,7 @@ int main(int argc, char* argv[]) {
 
 			}
 
-
 		}
-
 
 	}
 
